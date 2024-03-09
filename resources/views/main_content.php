506ee5e1969a50c7d1bcf86061536bd42d27e239
LukePadawalker <padurariu.luca@gmail.com>
2024-03-09 18:03:21 +0100

    page finished

--- resources/views/main_content.php
+++
@@ -1,8 +0,0 @@
-<div>
-    <div class="jumbotron">
-
-    </div>
-    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Hic quisquam consequuntur sit, fugiat molestias
-        accusantium expedita atque omnis sequi, necessitatibus in officiis nobis, quos labore? Aliquam perspiciatis
-        commodi nihil natus?</p>
-</div>
